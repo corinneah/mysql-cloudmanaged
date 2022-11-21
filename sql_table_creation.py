a507ff91fa3e588743ac887551599f5128f6d1da
corinneah <103472876+corinneah@users.noreply.github.com>
2022-11-21 18:49:39 -0500

    Update sql_table_creation.py

--- sql_table_creation.py
+++ sql_table_creation.py
@@ -1,1 +1,77 @@
+#import packages
+import pandas as pd
+import dbm
+import sqlalchemy
+from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv 
 
+load_dotenv()
+
+MYSQL_HOSTNAME = os.getenv ('MYSQL_HOSTNAME')
+MYSQL_USER = os.getenv ('MYSQL_USER')
+MYSQL_PASSWORD = os.getenv ('MYSQL_PASSWORD')
+MYSQL_DATABASE = os.getenv ('MYSQL_DATABASE')
+
+connection_string = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOSTNAME}:3306/{MYSQL_DATABASE}'
+db = create_engine(connection_string)
+print (db.table_names())
+
+table_patients = """
+create table if not exists patients (
+    id int auto_increment,
+    mrn varchar(255) default null unique,
+    first_name varchar(255) default null,
+    last_name varchar(255) default null,
+    zip_code varchar(255) default null,
+    dob varchar(255) default null,
+    gender varchar(255) default null,
+    pronouns varchar(255) default null,
+    insurance varchar(255) default null,
+    phone_number varchar(255) default null,
+    PRIMARY KEY (id) 
+); 
+"""
+
+table_medications = """
+create table if not exists medications (
+    id int auto_increment,
+    mrn varchar(255) default null,
+    med_ndc varchar(255) default null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (mrn) REFERENCES patients(mrn) ON DELETE CASCADE);  
+    """
+
+table_treatment_process = """
+create table if not exists treatment_process(
+    id int auto_increment,
+    mrn varchar(255) default null unique,
+    treatment_name varchar(255) default null,
+    treatment_type varchar(255) default null,
+    PRIMARY KEY (id) 
+); 
+"""
+table_patient_conditions = """
+create table if not exists patient_conditions (
+    id int auto_increment,
+    mrn varchar(255) default null,
+    icd10_code varchar(255) default null,
+    icd10_descriptions varchar(255) default null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (mrn) REFERENCES patients(mrn) ON DELETE CASCADE ); """
+
+table_social_determinants = """
+create table if not exists social_determinants(
+    id int auto_increment,
+    LOINC_NUM varchar(255) default null,
+    COMPONENT varchar(255) default null,
+    PRIMARY KEY (id)
+);
+"""
+db.execute(table_patients)
+db.execute(table_medications)
+db.execute(table_treatment_process)
+db.execute(table_patient_conditions)
+db.execute(table_social_determinants)
+
+gcp_tables = db.table_names()
